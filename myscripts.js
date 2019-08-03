30ec409dc2cdd7ccdcb2bdd7d7797e5439ad6387
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-08-03 10:32:06 -0500

    Final styling commits for MentorMe. Removed integration of JS nav bar because it was not styled correctly and would interfere with the look of the site as it was.

---
+++ myscripts.js
@@ -0,0 +1,19 @@
+//Unable to fix formatting in the allotted time. The logic works and it creates a sticky nav bar,
+//but additional formatting would be required to make it work in tablet and mobile views. I'm 
+//keeping the code in for reference. -ST
+
+
+// const navBar = document.getElementById("#main");
+// let topOfNav = navBar.offsetTop;
+
+// function fixNav() {
+//     if (window.scrollY >= topOfNav) {
+//         document.body.style.paddingTop = navBar.offsetHeight + 'px';
+//         document.body.classList.add('fixed-nav');
+//     } else {
+//         document.body.style.paddingTop = 0;
+//         document.body.classList.remove('fixed-nav');
+//     }
+// }
+
+// window.addEventListener('scroll', fixNav);
